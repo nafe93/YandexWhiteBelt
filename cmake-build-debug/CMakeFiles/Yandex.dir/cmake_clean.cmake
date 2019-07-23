1cc7742e6986d239fb48c01b84e3c907aeaf9676
developer <nafe93@windowslive.com>
2019-07-23 02:32:23 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Yandex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Yandex.dir/main.cpp.o"
+  "Yandex.pdb"
+  "Yandex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Yandex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
